9225d5e17a630d2b2d81691259a8cf7c9c530f0c
Kunli <dengkunli@foxmail.com>
2016-02-03 17:58:29 +0100

    d3 test

--- rope.js
+++ rope.js
@@ -39,6 +39,55 @@
         return values[values.length() / 2];
     }
 
+    function drawChart() {
+        var indicator,
+            lsoa11cd_left = PCDtoLSOA11CD($("#left_postcode_rope").get()[0].value.toUpperCase()),
+            lsoa11cd_right = PCDtoLSOA11CD($("#right_postcode_rope").get()[0].value.toUpperCase());
+
+        if (!lsoa11cd_left || !lsoa11cd_right) {
+            return;
+        };
+
+        for (indicator in INDICATORS) {
+            //console.log(d3.select('#compchart').style('width'));
+            var margin = {top: 0, right: 10, bottom: 0, left: 10}
+                , width = parseInt(d3.select('#compchart').style('width'), 10) / 3
+                , width = width - margin.left - margin.right
+                , barHeight = 20
+                , percent = d3.format('%');
+
+            // scales and axes
+            var x = d3.scale.linear()
+                .range([0, width])
+                .domain([0, 10]); // hard-coding this because I know the data
+
+            //console.log(window.data[lsoa11cd_left][indicator]["decile"]);
+
+            var svgl = d3.select("#"+indicator+"-left").append("svg")
+                .attr("width", width)
+                .attr("height", barHeight)
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('style', 'fill : white')
+                .attr('x', '0')
+                .attr('width', x(window.data[lsoa11cd_left][indicator]["decile"]))
+                .attr('y', '0')
+                .attr('height', '50');
+
+            var svgr = d3.select("#"+indicator+"-right").append("svg")
+                .attr("width", width)
+                .attr("height", barHeight)
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('style', 'fill : white')
+                .attr('x', '0')
+                .attr('width', x(window.data[lsoa11cd_right][indicator]["decile"]))
+                .attr('y', '0')
+                .attr('height', '50');
+        }
+    }
+    drawChart();
+
     function updateChart(which) {
         var indicator,
             data,
@@ -89,8 +138,8 @@
         }
     }
 
-    $("#left_postcode_rope").change(updateChart.bind(undefined, "left"));
-    $("#right_postcode_rope").change(updateChart.bind(undefined, "right"));
+    $("#left_postcode_rope").change(drawChart);
+    $("#right_postcode_rope").change(drawChart);
     $("#which_left").change(onSelectChange.bind(undefined, "left"));
     $("#which_right").change(onSelectChange.bind(undefined, "right"));
 }());
